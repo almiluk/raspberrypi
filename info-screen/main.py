e7f3a29f73dbbb053756f11f4c52d42a5caf6d88
almiluk <almiluk@gmail.com>
2023-09-17 15:25:19 +0300

    work with pcd8544

--- info-screen/main.py
+++ info-screen/main.py
@@ -1,5 +1,39 @@
 from rpi_informer import RPIInformer
+from pcd8544 import PCB8544
 
-informer = RPIInformer()
-informer.Tick()
+import board
+import signal
+from time import sleep
 
+
+def main():
+    informer = RPIInformer()
+    display = PCB8544(
+        board.D6,
+        board.D5,
+        board.D9,
+        50
+    )
+
+    def print_rpi_info(informer: RPIInformer):
+        display.fill(0)
+        display.text("test", 0, 0, 1)
+        display.show()
+
+    informer.AddRule(
+        lambda x: True,
+        print_rpi_info,
+    )
+
+    def interrupt_handler(signum, frame):
+        pass
+
+    signal.signal(signal.SIGINT, interrupt_handler)
+
+    while True():
+        RPIInformer.Tick()
+        sleep(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ info-screen/pcd8544/pcd8544.py
@@ -0,0 +1,24 @@
+from adafruit_pcd8544 import PCD8544
+import busio
+import board
+import digitalio
+from adafruit_blinka.microcontroller.generic_linux.libgpiod_pin import Pin
+
+
+class PCB8544(PCD8544):
+    def __init__(self, dc_pin: Pin, cs_pin: Pin, reset_pin: Pin, backlight_pin: Pin, contrast: int = 80, bias: int = 4):
+        # Initialize SPI bus and control pins
+        spi = busio.SPI(board.SCK, MOSI=board.MOSI)
+        dc = digitalio.DigitalInOut(dc_pin)                 # data/command
+        cs = digitalio.DigitalInOut(cs_pin)                 # Chip select
+        reset = digitalio.DigitalInOut(reset_pin)           # reset
+        self.__backlight = digitalio.DigitalInOut(backlight_pin)   # backlight
+        self.__backlight.switch_to_output()
+
+        super().__init__(spi, dc, cs, reset, contrast=contrast, bias=bias)
+
+        self.fill(0)
+        self.show()
+
+    def SetBacklight(self, on: bool) -> None:
+        self.__backlight.value = on
