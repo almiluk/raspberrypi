f0510d643e8327d456ac03c47b284feb01551980
almiluk <almiluk@gmail.com>
2023-09-18 18:13:45 +0300

    info-screen: show more info on a screen

--- info-screen/main.py
+++ info-screen/main.py
@@ -20,14 +20,25 @@ def main():
     )
 
     def print_rpi_info(informer: RPIInformer):
+        template = (""
+                    + "({bitrate}){ssid}\n"
+                    + "CP: {cpu:>3} T: {temp:>3}\n"
+                    + "MEM:  {mem_used:>4}/{mem_total:>4} MB\n"
+                    + "DISK: {disk_used:>3}/{disk_total:>3} GB\n"
+                    )
+        msg = template.format(
+            bitrate=int(informer.WiFiInfo.bit_rate),
+            ssid=informer.WiFiInfo.ssid,
+            cpu=int(informer.CPUInfo.percent),
+            temp=int(informer.TempInfo.cpu_temp),
+            mem_used=int(informer.MemInfo.used / 1024 / 1024),
+            mem_total=int(informer.MemInfo.total / 1024 / 1024),
+            disk_used=int(informer.DiskInfo.used / 1024 / 1024 / 1024),
+            disk_total=int(informer.DiskInfo.total / 1024 / 1024 / 1024),
+        )
 
         # TODO: Move it to PCB8544 class
         display.fill(0)
-        msg = (""
-        + "({bitrate}){ssid}\n"
-        + "{cpu}"
-        ).format(ssid=informer.WiFiInfo.ssid, bitrate=int(informer.WiFiInfo.bit_rate), cpu=informer.CPUInfo.percent)
-
         image = Image.new("1", (display.width, display.height))
         draw = ImageDraw.Draw(image)
         font = ImageFont.truetype("cg-pixel-4x5-mono.ttf", 5)
